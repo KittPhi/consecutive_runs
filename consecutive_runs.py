d7c4459a137a560d3bb544b0246eb861baec8f1a
haiderali786 <haiderratlamwala786@gmail.com>
2021-05-05 11:19:21 +0530

    Removed underscores(_)

--- consecutive_runs.py
+++ consecutive_runs.py
@@ -13,67 +13,67 @@ def find_consecutive_runs(input_numbers: List) -> Optional[List[int]]:
     Returns: Optional[List[int]]
 
     """
-    _first_elements_in_consecutive_runs = []
+    first_elements_in_consecutive_runs = []
 
     # This can be modified to be a parameter to change count of numbers
     # in a consecutive run to make the code reusable/scalable
-    _count_of_numbers_in_run = 3
-    _min_index_for_run = _count_of_numbers_in_run - 1
+    count_of_numbers_in_run = 3
+    minindex_for_run = count_of_numbers_in_run - 1
 
     # Loop over the input to find indices of the first element of consecutive run
-    for _index, _number in enumerate(input_numbers):
-        if _index >= _min_index_for_run:
+    for index, number in enumerate(input_numbers):
+        if index >= minindex_for_run:
 
             # Slice the input_numbers to get the run
-            _run = _slice_input_numbers(input_numbers, _index, _count_of_numbers_in_run)
+            run = slice_input_numbers(input_numbers, index, count_of_numbers_in_run)
 
             # Check if the numbers in the run are consecutive that increase or decrease by 1
-            _is_consecutive = _is_run_consecutive(_run)
+            is_consecutive = is_run_consecutive(run)
 
             # Add list indices of the first element of each consecutive run
-            if _is_consecutive:
-                _index_of_first_number_in_run = _index - 2
-                _first_elements_in_consecutive_runs.append(_index_of_first_number_in_run)
+            if is_consecutive:
+                index_of_first_number_in_run = index - 2
+                first_elements_in_consecutive_runs.append(index_of_first_number_in_run)
 
     # Results in returning None if there are no consecutive runs
-    if _first_elements_in_consecutive_runs:
-        return _first_elements_in_consecutive_runs
+    if first_elements_in_consecutive_runs:
+        return first_elements_in_consecutive_runs
 
 
-def _slice_input_numbers(input_numbers: List, _index: int, _count_of_numbers_in_run: int) -> List[int]:
+def slice_input_numbers(input_numbers: List, index: int, count_of_numbers_in_run: int) -> List[int]:
     """
-    Slice _count_of_numbers_in_run of input_numbers ending at index: _index
+    Slice count_of_numbers_in_run of input_numbers ending at index: index
     Args:
         input_numbers (): List
-        _index (): int
-        _count_of_numbers_in_run (): int
+        index (): int
+        count_of_numbers_in_run (): int
 
     Returns: List[int]
 
     """
-    _end = _index + 1
-    _start_index = _end - _count_of_numbers_in_run
+    end = index + 1
+    start = end - count_of_numbers_in_run
 
-    return input_numbers[_start_index:_end]
+    return input_numbers[start:end]
 
 
-def _is_run_consecutive_with_direction(numbers_in_run: List, _direction: int) -> bool:
+def is_run_consecutive_with_direction(numbers_in_run: List, direction: int) -> bool:
     """
     Checks if the numbers in the run are consecutive in the given direction
     Args:
         numbers_in_run (): List
-        _direction (): int
+        direction (): int
 
     Returns: bool
 
     """
-    _prev_number = None
+    prev_number = None
 
-    for _number in numbers_in_run:
-        if _prev_number is None:
-            _prev_number = _number
-        elif _number == _prev_number + _direction:
-            _prev_number = _number
+    for number in numbers_in_run:
+        if prev_number is None:
+            prev_number = number
+        elif number == prev_number + direction:
+            prev_number = number
         else:
             # _prev_number and _number are not consecutive
             return False
@@ -81,7 +81,7 @@ def _is_run_consecutive_with_direction(numbers_in_run: List, _direction: int) ->
     return True
 
 
-def _is_run_positive_consecutive(numbers_in_run: List) -> bool:
+def is_run_positive_consecutive(numbers_in_run: List) -> bool:
     """
     Checks if the numbers in the run are consecutive in the positive direction
     Args:
@@ -90,10 +90,10 @@ def _is_run_positive_consecutive(numbers_in_run: List) -> bool:
     Returns: bool
 
     """
-    return _is_run_consecutive_with_direction(numbers_in_run, 1)
+    return is_run_consecutive_with_direction(numbers_in_run, 1)
 
 
-def _is_run_negative_consecutive(numbers_in_run: List) -> bool:
+def is_run_negative_consecutive(numbers_in_run: List) -> bool:
     """
     Checks if the numbers in the run are consecutive in the negative direction
     Args:
@@ -102,10 +102,10 @@ def _is_run_negative_consecutive(numbers_in_run: List) -> bool:
     Returns: bool
 
     """
-    return _is_run_consecutive_with_direction(numbers_in_run, -1)
+    return is_run_consecutive_with_direction(numbers_in_run, -1)
 
 
-def _is_run_consecutive(numbers_in_run: List) -> bool:
+def is_run_consecutive(numbers_in_run: List) -> bool:
     """
     Checks if the numbers in the run are consecutive
     Args:
@@ -114,9 +114,9 @@ def _is_run_consecutive(numbers_in_run: List) -> bool:
     Returns: bool
 
     """
-    if _is_run_positive_consecutive(numbers_in_run):
+    if is_run_positive_consecutive(numbers_in_run):
         return True
-    elif _is_run_negative_consecutive(numbers_in_run):
+    elif is_run_negative_consecutive(numbers_in_run):
         return True
     else:
         return False
